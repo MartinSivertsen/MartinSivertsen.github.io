44bdc644a6b4e1080925155784d9f8f116c1a0f1
MartinSivertsen <martisiv@student.hist.no>
2016-02-12 15:05:20 +0100

    Change name in footer of js-file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
+"<p>&copy;  " + new Date().getFullYear() + " Martin Sivertsen & Co. Alle rettigheter</p>";
 
 document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
