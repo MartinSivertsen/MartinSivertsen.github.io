fcd32aff94dad1bb546c8e3c26fdef9d331226bc
MartinSivertsen <martisiv@student.hist.no>
2016-02-12 16:36:32 +0100

    Add Mobile in js-file

--- script.js
+++ script.js
@@ -6,4 +6,5 @@ document.getElementById("nav01").innerHTML =
 "<li><a href='index.html'>Home</a></li>" +
 "<li><a href='customers.html'>Data</a></li>" +
 "<li><a href='about.html'>About</a></li>" +
+"<li><a href='about.html'>Mobile</a></li>" +
 "</ul>";
